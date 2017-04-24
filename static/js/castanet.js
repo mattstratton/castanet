0cfbb1ce4748e3009beac4f5099860d281272c12
Matt Stratton <matt.stratton@gmail.com>
2017-04-23 23:23:36 -0500

    Switch to audio-js from bootstrap3 player

--- static/js/castanet.js
+++ static/js/castanet.js
@@ -2,4 +2,11 @@
 // @codekit-prepend '../../bower_components/tether/dist/js/tether.js'
 // @codekit-prepend '../../bower_components/bootstrap/dist/js/bootstrap.js'
 // @codekit-prepend '../../bower_components/jssocials/dist/jssocials.js'
-// @codekit-prepend 'bootstrap3_player.js'
+// @codekit-prepend '../../bower_components/audiojs/audiojs/audio.js'
+
+audiojs.events.ready(function() { 
+  var as = audiojs.createAll({
+    imageLocation: '/images/player-graphics.gif',
+    swfLocation: '/swf/audiojs.swf'
+   });
+ });
