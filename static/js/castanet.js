a6bc4dda2d48bc7637140179bfa39a9a29c9027b
Matt Stratton <matt.stratton@gmail.com>
2017-04-23 19:26:09 -0500

    Set up basic bootstrap 4

---
+++ static/js/castanet.js
@@ -0,0 +1,5 @@
+// @codekit-prepend '../../bower_components/jquery/dist/jquery.js'
+// @codekit-prepend '../../bower_components/tether/dist/js/tether.js'
+// @codekit-prepend '../../bower_components/bootstrap/dist/js/bootstrap.js'
+// @codekit-prepend '../../bower_components/jssocials/dist/jssocials.js'
+// @codekit-prepend 'bootstrap3_player.js'
