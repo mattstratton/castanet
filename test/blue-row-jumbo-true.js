00f4fd41c95fd5663d8756974edb8089a8510234
Matt Stratton <matt.stratton@hey.com>
2020-12-09 16:39:36 -0600

    Minimize lighthouse runs
    Signed-off-by: Matt Stratton <matt.stratton@hey.com>

--- test/blue-row-jumbo-true.js
+++ test/blue-row-jumbo-true.js
@@ -2,10 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: [
-        'http://localhost:1313/',
-        'http://localhost:1313/aug/',
-        'http://localhost:1313/guest/',
-        'http://localhost:1313/about/'
+        'http://localhost:1313/'
         ],
       startServerCommand: 'cd exampleSite && HUGO_ENV=production && hugo server --config ../test/blue-row-jumbo-true-config.toml --baseUrl="http://localhost:1313"',
     },
