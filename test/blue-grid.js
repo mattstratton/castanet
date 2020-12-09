a129a06e3258b205ddcffe6098be88e716858aa3
Matt Stratton <matt.stratton@hey.com>
2020-12-09 16:18:27 -0600

    More actions testing
    Signed-off-by: Matt Stratton <matt.stratton@hey.com>

--- test/blue-grid.js
+++ test/blue-grid.js
@@ -8,6 +8,7 @@ module.exports = {
         'http://localhost:1313/about/'
         ],
       startServerCommand: 'cd exampleSite && HUGO_ENV=production && hugo server --config ../test/blue-grid-config.toml --baseUrl="http://localhost:1313"',
+      startServerReadyPattern: 'Web Server is available'
     },
     upload: {
       target: 'temporary-public-storage',
