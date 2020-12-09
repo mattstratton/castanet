6602a86008be1b2a6edcb4f63bf2bde80d67fe9d
Matt Stratton <matt.stratton@hey.com>
2020-12-09 15:57:08 -0600

    Test with matrix build
    Signed-off-by: Matt Stratton <matt.stratton@hey.com>

---
+++ test/blue-row-jumbo-false.js
@@ -0,0 +1,16 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        'http://localhost:1313/',
+        'http://localhost:1313/aug/',
+        'http://localhost:1313/guest/',
+        'http://localhost:1313/about/'
+        ],
+      startServerCommand: 'cd exampleSite && HUGO_ENV=production && hugo server --config ../test/blue-row-jumbo-false-config.toml --baseUrl="http://localhost:1313"',
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
