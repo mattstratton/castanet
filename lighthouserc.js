28d35b13a5a11df7a99b1d935cfb8ad22e930d37
Matt Stratton <matt.stratton@hey.com>
2020-12-09 13:21:33 -0600

    A few more actions tweaks
    Signed-off-by: Matt Stratton <matt.stratton@hey.com>

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: ['http://localhost:1313/'],
-      startServerCommand: 'cd exampleSite && hugo server --baseUrl="http://localhost:1313"',
+      startServerCommand: 'cd exampleSite && HUGO_ENV=production && hugo server --baseUrl="http://localhost:1313"',
     },
     upload: {
       target: 'temporary-public-storage',
