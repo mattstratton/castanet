cfb6d4582410a8fc97decbefcee000336b5dd833
Matt Stratton <matt.stratton@gmail.com>
2025-05-14 10:42:06 -0500

    Update CI

--- test/grid.js
+++ test/grid.js
@@ -7,7 +7,7 @@ module.exports = {
         'http://localhost:1313/guest/',
         'http://localhost:1313/about/'
         ],
-      startServerCommand: 'cd exampleSite && HUGO_ENV=production && hugo server --config ../test/blue-grid-config.toml --baseUrl="http://localhost:1313"',
+      startServerCommand: 'cd exampleSite && HUGO_ENV=production && hugo mod get -u && hugo server --configDir ../test/grid --baseUrl="http://localhost:1313"',
       startServerReadyPattern: 'Web Server is available'
     },
     upload: {
