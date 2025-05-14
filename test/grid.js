d80da0aa5690f85bc7186c4cab98f40e1117d314
Matt Stratton <matt.stratton@gmail.com>
2025-05-14 10:45:10 -0500

    Fix lighthouse scripts

--- test/grid.js
+++ test/grid.js
@@ -7,7 +7,7 @@ module.exports = {
         'http://localhost:1313/guest/',
         'http://localhost:1313/about/'
         ],
-      startServerCommand: 'cd exampleSite && HUGO_ENV=production && hugo mod get -u && hugo server --configDir ../test/grid --baseUrl="http://localhost:1313"',
+      startServerCommand: 'cd exampleSite && HUGO_ENV=production && hugo mod get -u && hugo server --configDir ../test/grid --baseURL="http://localhost:1313"',
       startServerReadyPattern: 'Web Server is available'
     },
     upload: {
--- test/row-jumbo-false.js
+++ test/row-jumbo-false.js
@@ -4,7 +4,7 @@ module.exports = {
       url: [
         'http://localhost:1313/'
         ],
-      startServerCommand: 'cd exampleSite && HUGO_ENV=production && hugo mod get -u && hugo server --configDir ../test/row-jumbo-false --baseUrl="http://localhost:1313"',
+      startServerCommand: 'cd exampleSite && HUGO_ENV=production && hugo mod get -u && hugo server --configDir ../test/row-jumbo-false --baseURL="http://localhost:1313"',
     },
     upload: {
       target: 'temporary-public-storage',
